c29492fe5df46814c046ea45494ba041b3fec37b
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2023-11-20 10:41:49 +0300

    interim results

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
+function initializeBoard() {
+    const boardElement = document.getElementById("board");
+
+    for (let i = 7; i >= 0; i--) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let j = 0; j < 8; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.id = `${String.fromCharCode(97 + j)}${8 - i}`;
+
+            if (boardState[i][j] !== "") {
+                const piece = document.createElement("div");
+                piece.classList.add("piece", boardState[i][j]);
+                piece.setAttribute("draggable", "true");
+                piece.addEventListener("dragstart", handleDragStart);
+                square.appendChild(piece);
+            }
+
+            square.addEventListener("dragover", handleDragOver);
+            square.addEventListener("drop", handleDrop);
+
+            row.appendChild(square);
+        }
+
+        boardElement.appendChild(row);
+    }
+}
+
 function handleMove(sourceSquare, targetSquare) {
     const sourceElement = document.getElementById(sourceSquare);
     const targetElement = document.getElementById(targetSquare);
 
     const pieceClass = sourceElement.querySelector(".piece").classList[1];
-    sourceElement.querySelector(".piece").classList.remove(pieceClass);
     targetElement.innerHTML = "";
 
     const newPiece = document.createElement("div");
     newPiece.classList.add("piece", pieceClass);
     targetElement.appendChild(newPiece);
+
+    sourceElement.querySelector(".piece").classList.remove(pieceClass);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
